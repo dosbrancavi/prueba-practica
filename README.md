b5d14e10d4b2aaa79a3500684a6911590a7d9a9b
brandoncfridivi <96086960+brandoncfridivi@users.noreply.github.com>
2024-06-27 00:08:31 -0600

    Ultimo commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# prueba-practica
+## Aplicacion de tareas
+
+Para correr la aplicación en cualquier ambiente primero debemos:
+
+- Posicionarse en la ruta **prueba-practica**
+- Tener docker desktop instalado 
+- Ejecutar el comando **docker-compose up --build**
+- Una vez que el contenedor levante ir a la ruta prueba-practica/backend **prueba-practica/backend**
+- Ejecutar el comando **./mvnw quarkus:dev**
+
+
+## Funciones de la Aplicación
+- Al Primer ingreso muestra **wizard-guide**
+- Permite la **creación de usuario** 
+- Permite el **inicio de sesión** de los usuarios
+- Dentro de la sesión permite **crear tareas**
+- Si hay tareas creadas permite hacer **drag and drop entre columnas** por estado
+- Al dar click en una tarea permite **editarla o eliminarla** 
+- **Perfil de usuario** al clikear en el nombre o en la foto
+- Permite **cerrar sesión** desde el icono de logout en tareas o desde el botón cerrar sesión en el perfil
--- frontend/src/app/components/new-task-modal/new-task-modal.component.ts
+++ frontend/src/app/components/new-task-modal/new-task-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from "@angular/core";
+import { ChangeDetectorRef, Component, ElementRef, ViewChild, inject } from "@angular/core";
 import {
   FormBuilder,
   FormGroup,
@@ -7,7 +7,7 @@ import {
 } from "@angular/forms";
 import { DataService } from "../../services/data.service";
 import { CreateTask, Task } from "../../interfaces/task.interface";
-import { MatDialog, MatDialogRef } from "@angular/material/dialog";
+import { MatDialogRef } from "@angular/material/dialog";
 import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from "@angular/material/button";
 import { MatGridListModule } from "@angular/material/grid-list";
@@ -23,32 +23,44 @@ import { Router } from "@angular/router";
     MatButtonModule,
     MatGridListModule,
     MatSelectModule,
+    
   ],
   templateUrl: "./new-task-modal.component.html",
   styleUrl: "./new-task-modal.component.css",
 })
 export class NewTaskModalComponent {
+  @ViewChild("imageCanvas") canvas!: ElementRef<HTMLCanvasElement>;
+
   selectedFile: File | null = null;
   selectedFileName: string | null = null;
   token = "";
-  id = null;
-  router: Router = inject(Router)
+  id: string | null = null;
 
+  // Variable para controlar el modo de recorte
+  isCropping = false;
+
+  // Variables para el recorte
+  cropX = 0;
+  cropY = 0;
+  cropWidth = 0;
+  cropHeight = 0;
 
   constructor(
     private formBuilder: FormBuilder,
     private dataService: DataService,
-    public dialogRef: MatDialogRef<NewTaskModalComponent>
+    private router: Router,
+    private dialogRef: MatDialogRef<NewTaskModalComponent>,
+    private cdr: ChangeDetectorRef // Inject ChangeDetectorRef for manual change detection
   ) {}
 
   createTaskForm: FormGroup = this.formBuilder.group({
-    description: ["", Validators.required],
-    status: ["Pendiente", Validators.required],
+    description: ["", [Validators.required, Validators.maxLength(100)]],
+    status: ["Pendiente", [Validators.required]],
     imageUrl: [""],
   });
 
   get description() {
-    return this.createTaskForm.get("description");
+    return this.createTaskForm.controls["description"];
   }
   get status() {
     return this.createTaskForm.get("status");
@@ -65,8 +77,87 @@ export class NewTaskModalComponent {
       const reader = new FileReader();
       reader.onload = () => {
         this.createTaskForm.patchValue({ imageUrl: reader.result });
+        this.renderImage();
       };
       reader.readAsDataURL(this.selectedFile);
+
+      // Activar el modo de recorte al seleccionar un archivo
+      this.startCropping();
+    }
+  }
+
+  renderImage() {
+    const reader = new FileReader();
+    reader.onload = () => {
+      const img = new Image();
+      img.src = reader.result as string;
+      img.onload = () => {
+        const canvas = this.canvas.nativeElement;
+        const ctx = canvas.getContext("2d");
+        if (ctx) {
+          canvas.width = img.width;
+          canvas.height = img.height;
+          ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+          // Establecer el área de recorte inicial con aspecto 1:1
+          const size = Math.min(canvas.width, canvas.height);
+          this.cropWidth = size;
+          this.cropHeight = size;
+          this.cropX = (canvas.width - size) / 2;
+          this.cropY = (canvas.height - size) / 2;
+          // Dibujar el área de recorte inicial
+          this.drawCropArea();
+
+          // Manually trigger change detection to update the view
+          this.cdr.detectChanges();
+        }
+      };
+    };
+    reader.readAsDataURL(this.selectedFile!);
+  }
+
+  drawCropArea() {
+    const canvas = this.canvas.nativeElement;
+    const ctx = canvas.getContext("2d");
+    if (ctx) {
+      ctx.strokeStyle = "red";
+      ctx.lineWidth = 2;
+      ctx.strokeRect(this.cropX, this.cropY, this.cropWidth, this.cropHeight);
+    }
+  }
+
+  startCropping() {
+    this.isCropping = true;
+  }
+
+  cancelCropping() {
+    this.isCropping = false;
+    // Limpiar variables y reiniciar el formulario si es necesario
+    this.selectedFile = null;
+    this.selectedFileName = null;
+    this.createTaskForm.reset();
+  }
+
+  finalizeCropping() {
+    this.isCropping = false;
+    // Recortar la imagen según los valores de recorte y volver a mostrar el formulario
+    this.cropImage();
+  }
+
+  cropImage() {
+    const canvas = this.canvas.nativeElement;
+    const ctx = canvas.getContext("2d");
+    if (ctx) {
+      const imageData = ctx.getImageData(
+        this.cropX,
+        this.cropY,
+        this.cropWidth,
+        this.cropHeight
+      );
+      canvas.width = this.cropWidth;
+      canvas.height = this.cropHeight;
+      ctx.putImageData(imageData, 0, 0);
+      // No enviar la imagen recortada automáticamente, solo actualizar el canvas
+      // Aquí podrías emitir un evento o realizar alguna acción adicional si es necesario
     }
   }
 
@@ -74,33 +165,46 @@ export class NewTaskModalComponent {
     this.dialogRef.close();
   }
 
-  create(): void {
-    if (this.createTaskForm.valid) {
+  create() {
+    if (this.createTaskForm.valid && !this.isCropping) {
       const user = JSON.parse(localStorage.getItem("user")!);
       this.token = user.csrfToken;
       this.id = user.user.id;
 
+      let imageToUse: File | null = this.selectedFile; // Por defecto, usa la imagen original
+
+      if (this.isCropping && this.canvas.nativeElement.toBlob) {
+        // Si se está recortando y es posible obtener una imagen recortada
+        this.canvas.nativeElement.toBlob((blob) => {
+          if (blob) {
+            imageToUse = new File([blob], this.selectedFileName!);
+          }
+        });
+      }
+
       const body: CreateTask = {
         description: this.description!.value,
         status: this.status!.value,
-        imageFile: this.selectedFile,
-        user: { id: this.id! },
+        imageFile: imageToUse,
+        user: { id: Number(this.id!) },
       };
 
       this.dataService.createTask(body, this.token).subscribe({
         next: (res) => {
-          this.close();
+          console.log("Tarea creada exitosamente:", res);
+          this.close(); // Cierra el modal si es necesario
         },
         error: (err) => {
+          console.error("Error al crear la tarea:", err);
           if (err.status === 401) {
-             localStorage.removeItem('user');
-            this.router.navigate(['/login']);
-            this.close();
+            localStorage.removeItem("user");
+            this.router.navigate(["/login"]);
+            this.close(); // Cierra el modal si es necesario
           }
         },
       });
     } else {
-      console.error("El formulario no es válido.");
+      console.error("El formulario no es válido o estás en modo de recorte.");
     }
   }
 }
